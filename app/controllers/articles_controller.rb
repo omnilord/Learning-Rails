84a3160cb302a965008005085a71308953ae14b8
Omnilord <omnilord@privateerconsulting.com>
2016-06-08 09:33:11 -0400

    Creating a before action for article setting.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController  < ApplicationController
+
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def index
     @articles = Article.all
   end
@@ -8,7 +15,6 @@ class ArticlesController  < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -22,7 +28,6 @@ class ArticlesController  < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = 'Article successfully updated!'
       redirect_to article_path(@article)
@@ -32,14 +37,12 @@ class ArticlesController  < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "'#{@article.title}' Article successfully deleted."
     redirect_to articles_path
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   private
