ff67ba0e2352393d799b2da21f3494d21a3812c1
Omnilord <omnilord@privateerconsulting.com>
2016-08-05 06:25:04 -0400

    Cummulative update of view changes and some administrative maintenance.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'pages#index', layout: 'application'
+  get 'about' => 'pages#about'
 
   resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
