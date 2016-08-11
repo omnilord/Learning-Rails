f40927f6e77f653383edd282ce31c9d0b35dca72
Omnilord <omnilord@privateerconsulting.com>
2016-08-11 13:15:05 -0400

    Adding basic Users scaffold.

---
+++ db/migrate/20160811150728_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def up
+    create_table :users do |t|
+      t.timestamps
+      t.text :username
+      t.text :email
+    end
+
+    #add_column :articles, :user_id, :integer, index: true
+    add_foreign_key :articles, :users, on_delete: :restrict, on_update: :cascade
+  end
+
+  def down
+    drop_table :users, force: :cascade
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510215203) do
+ActiveRecord::Schema.define(version: 20160811150728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,4 +25,12 @@ ActiveRecord::Schema.define(version: 20160510215203) do
     t.integer  "user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "username"
+    t.text     "email"
+  end
+
+  add_foreign_key "articles", "users", on_update: :cascade, on_delete: :restrict
 end
