b2fa332e4349975e74e0141add00a9ddb2e22d97
Omnilord <omnilord@privateerconsulting.com>
2016-08-17 10:51:59 -0400

    Making a few style updates to make a couple of pages look similar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def avatar_for(user, opts = { size: 32 })
+    #id = Digest::MD5::hexdigest(user.email.downcase)
+    #url = "https://secure.gravatar.com/avatar/#{id}?s=#{opts[:size]}"
+    image_tag('privateerconsulting-logo.png', alt: 'B', width: opts[:size], height: opts[:size])
+  end
 end
