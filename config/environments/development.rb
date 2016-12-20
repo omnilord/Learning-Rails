382b1fc9f251fdb1d37720c54a98f70ecbf2cd2a
Omnilord <omnilord@privateerconsulting.com>
2016-12-20 14:08:31 -0500

    cleaning up session config files.  Specifically setting Redis for Dev and Test, but using cookie store for production since Heroku is asking for a credit card, even for the hobby_dev 'free' redis.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.session_store :redis_store, servers: "redis://localhost:6379/0/session", expires_in: 90.minutes
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Rails.application.config.session_store :cookie_store, key: '_stocksapp_session'
 end
