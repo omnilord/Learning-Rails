f0192530e16ef74e4fe75eacf67741beec33dcd4
Omnilord <omnilord@privateerconsulting.com>
2016-12-23 00:49:03 -0500

    Minor tweaks, adding a My Portfolio summary next to title.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,24 @@ class UsersController < AuthAppController
         format.html { redirect_to root_path }
         format.json { render json: { status: :unauthorized } }
       else
-        @stocks = UserStock.where('user_id = ?', @user.id).map do |tracking|
-          { stock: tracking.stock, track: tracking }
-        end
         format.html { render :portfolio }
-        format.json { render json: {
+        format.json {
+          value = 0
+          shares = 0
+          @stocks = UserStock.where('user_id = ?', @user.id).map do |tracking|
+            value += tracking.owned * tracking.stock.last_price
+            shares += tracking.owned
+            { stock: tracking.stock, track: tracking }
+          end
+          render json: {
             status: :ok,
-            data: @stocks
+            data: {
+              portfolio: {
+                shares: shares,
+                value: value
+              },
+              stocks: @stocks
+            }
           }
         }
       end
