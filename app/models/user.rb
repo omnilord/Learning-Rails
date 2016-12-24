51a92fa896a30c8cd67a035efb1f3b52af24604a
Omnilord <omnilord@privateerconsulting.com>
2016-12-24 02:44:28 -0500

    Adding friendship migration and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
 
+  has_many :friendships
+  has_many :friends, through: :friendships
+
   attr_accessor :mfa # virtual field for sign-up form to allow redirecting to Authy setup
 
   def fullname
