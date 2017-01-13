df313f83d013cd1e8dca9b884689af005304124a
Omnilord <omnilord@privateerconsulting.com>
2017-01-13 15:41:52 -0500

    Making some of the search form iconography more appealing?

--- app/assets/javascripts/search_toogle.js
+++ app/assets/javascripts/search_toogle.js
@@ -4,14 +4,15 @@ $(function () {
     var $this = $(this),
         $forms = $this.closest('.toggling-forms-parent').find('.toggling-form'),
         $active = $forms.filter(':not(.invis)'),
-        search = $this.data('searchfor');
+        search = $this.data('searchfor'),
+        moving;
 
     if (search != $active.data('searchfor')) {
       $active = $forms.toggleClass('invis', true).filter('[data-searchfor="' + search + '"]').removeClass('invis');
       $this.addClass('marked').closest('li').siblings().find('a.toggle-form').removeClass('marked');
-      $active.find('.btn-group').append(
-        $this.closest('.btn-group').find('button.dropdown-toggle, ul.dropdown-menu').remove()
-      );
+      moving = $this.closest('.btn-group').find('button.dropdown-toggle, ul.dropdown-menu').remove();
+      moving.filter('button.dropdown-toggle').find('i').replaceWith($this.find('i').clone());
+      $active.find('.btn-group').append(moving);
     }
   });
 });
