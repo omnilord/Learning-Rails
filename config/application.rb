3a3acd7d6f7dabdee23ea9025619a328d639aa9c
Omnilord <omnilord@privateerconsulting.com>
2017-01-25 03:01:15 -0500

    Setting up a better boilerplate.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Derp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Use JavaScript instead of CoffeeScript
+    config.generators.javascript_engine = :js
   end
 end
