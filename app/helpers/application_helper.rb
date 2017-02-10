4ef370475d5e4ec53a0d71900c113b0f1fdd62de
Omnilord <omnilord@privateerconsulting.com>
2017-02-10 15:47:39 -0500

    Refactoring flash message class helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def alert_class(type)
+    case type
+    when 'notice' then 'alert alert-info'
+    when 'alert' then 'alert alert-danger'
+    else "alert alert-#{type}"
+    end
+  end
 end
